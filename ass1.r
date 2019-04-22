43f6af0ed9370d5279dc2840a9613e4cbead68cf
rque0001 <User@DESKTOP-TOTEUBD>
2019-04-22 16:39:51 +1000

    add file

--- ass1.r
+++ ass1.r
@@ -7,4 +7,10 @@ gdata <- read.csv("C:\\Users\\User\\Google Drive\\Monash units\\2019 Sem 1\\FIT3
 Threads = aggregate(data.frame(count = gdata$ThreadID), list(value = gdata$ThreadID), length)
 Authors = aggregate(data.frame(count = gdata$AuthorID), list(value = gdata$AuthorID), length)
 with(gdata, plot(AuthorID,Tone , col =ThreadID ,pch=as.numeric(ThreadID), main = ("webforum data")))
-with(gdata, plot(Date,Tone , col =filter(gdata$AuthorID,AuthorID>=0) ,pch=as.numeric(AuthorID), main = ("webforum data")))
+with(gdata, plot(Date,money , col =filter(gdata$AuthorID,AuthorID>=0) , main = ("webforum data")))
+#qplot(carat, price, data = gdata, color = color, size =clarity, alpha = cut)
+dates <- as.Date(gdata$Date, "%Y-%m-%d")
+data=gdata
+data$year <- format(dates, "%Y")
+
+test=aggregate(data[7:30], data[31], mean)
